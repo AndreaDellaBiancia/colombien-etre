298c42812e77e9cafb2612d4c1d493bb0a596360
Della Biancia <andreadellabiancia@gmail.com>
2022-04-08 11:33:44 +0200

    Page ressources videos dynamisée

--- src/Controller/Front/SpiritualityController.php
+++ src/Controller/Front/SpiritualityController.php
@@ -2,7 +2,8 @@
 
 namespace App\Controller\Front;
 
-
+use App\Entity\VideoSpirituality;
+use App\Repository\VideoSpiritualityRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,10 +13,12 @@ class SpiritualityController extends AbstractController
     /**
      * @Route("/spiritualite/ressources-videos", name="spirit-video")
      */
-    public function video(): Response
+    public function video(VideoSpiritualityRepository $videoSpirituality): Response
     {
 
-        return $this->render('front/spirituality/video.html.twig');
+        return $this->render('front/spirituality/video.html.twig', [
+            'videos' => $videoSpirituality->findBy([], ['id' => 'DESC'])
+        ]);
     }
 
      
